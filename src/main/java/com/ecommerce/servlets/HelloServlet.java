b58da13cc0906135d631cae50e0a3da121fc8789
Popol <github.afiqqo@gmail.com>
2025-01-16 01:46:30 +0800

    backend stuff

---
+++ src/main/java/com/ecommerce/servlets/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.ecommerce.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().write("{\"message\": \"Hello from the backend!\"}");
+    }
+}
